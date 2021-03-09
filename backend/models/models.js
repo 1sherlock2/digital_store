fb0be61902c4e6fe97257164d1a30e7db4f46cfd
a.i.gazdiev <a.i.gazdiev@tmn3.etagi.com>
2021-03-09 21:03:28 +0500

    fix bug with jwt token

--- backend/models/models.js
+++ backend/models/models.js
@@ -4,12 +4,7 @@ const { DataTypes } = require('sequelize');
 const User = sequelize.define('user', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true },
-  password: {
-    type: DataTypes.STRING,
-    set(value) {
-      this.setDataValue('password', value);
-    },
-  },
+  bcryptpass: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: 'user' },
 });
 const Basket = sequelize.define('basket', {
--- backend/routes/userRouter/controllers.js
+++ backend/routes/userRouter/controllers.js
@@ -4,15 +4,6 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const statusCode = require('../../utils/responseValue');
 
-const secretKey = process.env.PRIVAT_KEY;
-
-const generateJwt = (email, secretKey) => {
-  jwt.sign({ email }, secretKey, {
-    algorithm: 'HS256',
-    expiresIn: '1h',
-  });
-};
-
 exports.register = async (req, res, next) => {
   try {
     const { email, password, role } = req.body;
@@ -25,9 +16,8 @@ exports.register = async (req, res, next) => {
     if (findUser) {
       return res.status(400).json({ message: 'This user was created' });
     }
-    const privatKey = process.env.PRIVAT_KEY;
-    const bcryptPass = bcrypt.hash(password, 10);
-    const user = await User.create({ email, bcryptPass, role });
+    const bcryptpass = await bcrypt.hash(password, 10);
+    const user = await User.create({ email, bcryptpass, role });
     const basket = await Basket.create({ where: { userId: user.id } });
     return statusCode(res, 200, { message: 'User has been created' });
   } catch (e) {
@@ -38,29 +28,32 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email }, raw: true });
     if (!user) {
       return statusCode(res, 400, {
         message: 'email or password is not correct',
       });
     }
-    const correctPass = bcrypt.compareSync(password, user.password);
-    console.log(correctPass);
+    const correctPass = bcrypt.compareSync(password, user.bcryptpass);
     if (!correctPass) {
       return statusCode(res, 400, {
         message: 'email or password is not correct',
       });
     }
-    const token = generateJwt(user.email, secretKey);
+    const secretKey = process.env.PRIVAT_KEY;
+    const token = await jwt.sign({ email }, secretKey, {
+      algorithm: 'HS256',
+      expiresIn: '1h',
+    });
     return statusCode(res, 200, { token, user: user.id });
   } catch (e) {
     console.log(e);
   }
 };
 
-exports.check = (req, res) => {
-  const token = generateJwt(req.email, secretKey);
-  return statusCode(res, 200, { token });
+exports.check = async (req, res) => {
+  const user = await User.findAll();
+  return statusCode(res, 200, { user });
 };
 
 exports.deleteOne = async (req, res) => {
