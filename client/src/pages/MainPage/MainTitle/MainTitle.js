042e686c77abb8e4531a098b18ac7fbdf8ef8a18
a.i.gazdiev <a.i.gazdiev@tmn3.etagi.com>
2021-03-14 00:27:28 +0500

    working over Menu select

--- client/src/pages/MainPage/MainTitle/MainTitle.js
+++ client/src/pages/MainPage/MainTitle/MainTitle.js
@@ -2,20 +2,20 @@ import React, { useEffect, useRef } from 'react';
 import './MainTitle.scss';
 
 const MainTitle = () => {
-  // const loadRef = useRef();
+  const loadRef = useRef();
 
-  // useEffect(() => {
-  //   const h1 = document.createElement('h1');
-  //   h1.innerHTML = '<h1>Умный магазин</h1>';
-  //   setTimeout(() => {
-  //     h1.style.transition = '5s';
-  //     loadRef.current.appendChild(h1);
-  //   }, 2000);
-  //   console.log(loadRef);
-  // }, []);
+  useEffect(() => {
+    //   const h1 = document.createElement('h1');
+    //   h1.innerHTML = '<h1>Умный магазин</h1>';
+    //   setTimeout(() => {
+    //     h1.style.transition = '5s';
+    //     loadRef.current.appendChild(h1);
+    //   }, 2000);
+    console.log(loadRef);
+  }, []);
 
   return (
-    <div className="main_title">
+    <div ref={loadRef} className="main_title">
       <h1>Умный магазин</h1>
       <span> Подберем за вас необходимый Вам товар</span>
     </div>
--- client/src/pages/MainPage/MenuSelect/MenuSelect.js
+++ client/src/pages/MainPage/MenuSelect/MenuSelect.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 import './MenuSelect.scss';
+
 const MenuSelect = () => {
+  const [changeClass, setChangeClass] = useState(false);
+
   return (
-    <div className="menu_select">
-      <div className="menu_select_title">Меню </div>
+    <div>
+      <div
+        onMouseEnter={() => setChangeClass(!changeClass)}
+        className="menu_select"
+      >
+        <div className="menu_select_title">
+          <span>Меню</span>
+        </div>
+      </div>
+      <div
+        className={classNames('menu_select_subtitle', {
+          change_subtitle: changeClass,
+        })}
+      ></div>
     </div>
   );
 };
