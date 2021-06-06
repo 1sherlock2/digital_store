968b4b18b6c718901bb7e4a42bf66006007490b0
a.i.gazdiev <a.i.gazdiev@tmn3.etagi.com>
2021-06-06 23:34:10 +0500

    testing getBoundingClientRect

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import BasketIcons from './BasletIcons/BasketIcons';
 import MainTitle from './MainTitle/MainTitle';
 import MenuSelect from './MenuSelect/MenuSelect';
 import './MainPage.scss';
 
 const MainPage = () => {
+  const clientRef = useRef(null);
+
+  useEffect(() => {
+    const clientEl = clientRef.current;
+    const coordinate = clientEl.getBoundingClientRect();
+    console.log(coordinate);
+    console.log(clientEl.hasChildNodes());
+    console.log(clientEl.childNodes);
+    const { x, y } = coordinate;
+  }, []);
+
   return (
     <div className="main_page">
-      <div>
+      <div ref={clientRef}>
         <BasketIcons />
       </div>
       <div>
